bcf0e692332404028c2bc8fd5e42706a12bae24f
Siddhipradamohapatro120503 <siddhipradamohapatro120503@gmail.com>
2024-09-29 20:31:19 +0530

    first commit

---
+++ frontend/src/pages/community.jsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useRef, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./style/community_style.css";
+import axios from 'axios';
+import CommunityCard from '../components/CommunityCard';
+import firebase from '../config.js';
+import CommunitySearch from '../components/CommunitySearch.jsx';
+
+const Community = () => {
+  const offcanvasRef = useRef(null);
+
+  const [communities, setCommunities] = useState([]);
+
+  useEffect(() => {
+    // Check if offcanvasRef is available before initializing the Offcanvas
+    if (offcanvasRef.current) {
+      // Importing Bootstrap JavaScript directly in React is not recommended
+      // You should consider using React Bootstrap components or alternative libraries for better integration
+      // If you still want to use Bootstrap JavaScript, you might need to import it differently depending on your build setup
+      // For example, if you are using Webpack, you can import it like this:
+      // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+      // Then initialize Offcanvas like this:
+      // new window.bootstrap.Offcanvas(offcanvasRef.current);
+    }
+  }, [offcanvasRef]);
+
+  function getCommunities() {
+    axios.get('http://localho.st:' + process.env.REACT_APP_BACKEND_PORT + '/get-communities', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        const data = response.data;
+        const communityData = data.sort((a, b) => b.createdAt - a.createdAt);
+        setCommunities(communityData);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+
+  useEffect(() => {
+    const user = firebase.auth().currentUser || JSON.parse(localStorage.getItem('user'));
+    if (!user) {
+      window.location.href = '/profile';
+      return;
+    }
+    getCommunities();
+  }, []);
+
+
+  return (
+    <div className='background'>
+    <nav className="navbar bg-body-tertiary fixed-top" data-bs-theme="dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Selenify</a>
+        <ul className="nav nav-underline">
+          <li className="nav-item">
+            <a className="nav-link active tabs" aria-current="page" href="#home">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link tabs" href="#news">News</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link tabs" href="#petition">Petition</a>
+          </li>
+        </ul>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel" ref={offcanvasRef}>
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Channels</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+              <CommunitySearch communities={communities} postJoin={getCommunities} />
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    {/* HOME */}
+    <section id="home" className="home-dg container">
+      <div className="row" data-bs-theme="dark">
+        <h2 className="heading">Channels</h2>
+          {
+            communities.map((community) => (
+              CommunityCard(community, getCommunities, firebase)
+            ))
+          }
+      </div>
+    </section>
+
+    {/* News */}
+    <section id="news" className="news">
+      <div className="news-up" data-bs-theme="dark">
+        <h2 className="heading">News</h2>
+        <div className="card mb-3">
+          <img src="https://dynaimage.cdn.cnn.com/cnn/digital-images/org/5d2dab1a-866f-460b-a3de-c2534a3bade9.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Flood in Pune</h5>
+            <p className="card-text">Massive flood in Pune. Can situation be any better?</p>
+            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div className="card mb-3">
+          <img src="https://images.thequint.com/thequint%2F2015-07%2F79265aaf-9294-4c30-bf59-57f4504365ce%2FImphal-Hero-Final.jpg?w=1200&auto=format%2Ccompress&ogImage=true" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Strike in Manipur</h5>
+            <p className="card-text">Female Marchers on Road to strike against...</p>
+            <p className="card-text"><small className="text-body-secondary">Last updated 8 mins ago</small></p>
+          </div>
+        </div>
+        <div className="card mb-3">
+          <img src="https://static01.nyt.com/images/2021/02/22/world/22Myanmar1-sub/merlin_184108077_dc441c99-11a9-4fb0-bebf-591a9de682b3-superJumbo.jpg?quality=90&auto=webp" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Are Good Days on its way?</h5>
+            <p className="card-text">As the elections get over, people hope for development.</p>
+            <p className="card-text"><small className="text-body-secondary">Last updated 21 mins ago</small></p>
+          </div>
+        </div>
+      </div> 
+    </section>
+
+    {/* Petition */}
+    <section id="petition" className="petition">
+      <h2 className="heading">Petition</h2>
+      <h3 className="text">Wanna Raise a Petition?</h3>
+      <h4 className="text">Gain a vote of 50k, and your petition will be noticed and filed by respective welfare societies and NGOs</h4>
+      <div className="d-grid gap-2 col-6 mx-auto h-100">
+        <button className="btn btn-danger" type="button">Raise Petition</button>
+      </div>
+    </section>
+    </div>
+  );
+}
+
+export default Community;
---
+++ frontend/src/pages/search.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './style/search.css'; // Import CSS file for styling
+
+function Search() {
+  const [query, setQuery] = useState('');
+  const [generatedContent, setGeneratedContent] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // State variable for loading indicator
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true); // Set loading to true when search begins
+
+    try {
+      const response = await fetch('http://localhost:8000/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: query }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log('Response:', data); // Log the response data
+
+      // Check if the response contains the expected structure
+      if (!data.response || typeof data.response !== 'string') {
+        throw new Error('Invalid response data structure');
+      }
+
+      setGeneratedContent(data.response);
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setIsLoading(false); // Set loading to false after search completes (whether successful or not)
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <div>
+      <section className="laws search" id="laws">
+        <h1 className="section-title">Laws</h1>
+        <div className="search_bar">
+          <form onSubmit={handleSubmit}>
+            <input type="text" id="searchInput" placeholder="Search laws..." onChange={handleInputChange} value={query} />
+            <button className="button dropbtn" type="submit" disabled={isLoading}>
+              {isLoading ? 'Loading...' : 'Search'} {/* Display "Loading..." when isLoading is true */}
+            </button>
+          </form>
+          <div className="laws__item">
+            <h2 color='white'>Laws Content:</h2>
+            <div className="markdown-content">
+              <ReactMarkdown>{generatedContent}</ReactMarkdown>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Search;
